7fdcf6f17803c476e4bee8d278fa23176c9193f4
sriharshabandla <sriharshabandla@gmail.com>
2019-10-01 16:51:21 +0530

    basic programs

---
+++ listcomprehention.py
@@ -0,0 +1,11 @@
+
+l=[i**2 for i in range(0,20) if i%2==0]
+print(l)
+
+
+
+l=['h',1,'e',2,'l',3,'l',4,'o',5,'w',6,'o',7,'r',8,'l',9,'d']
+print(l[::2])
+print(l[0:20:2])                     #slicing
+
+
---
+++ pemdas.py
@@ -0,0 +1,42 @@
+variableno1=2      #pascalcase
+VariableNo2=4      #camelcase
+variable_n0_3=70   #snakecase
+VARIABLE_NO_4=52   #snakecase
+
+print("variableno1",variableno1)
+print("",VariableNo2)
+print("",variable_n0_3)
+print("VARIABLE_NO_4",VARIABLE_NO_4)
+
+
+
+a=str(1)
+b='harsha'
+print(a+b)
+
+
+
+
+
+print =int(input(str('enter value''3'))+str(harsha))
+
+
+
+pens=['ball','ink','penpencile']
+# print(pens.index('penpencile'))
+# print(pens*5)
+# print(pens[-2])
+
+# pens[1]='colorpen'
+
+pens.append('multiple pen')
+print(pens)
+
+
+
+
+
+
+list=[1,2,3,[4,5,6],['hi','hello'],7,8,9]
+print(list[3][1])
+print(list[4][0])
